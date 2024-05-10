4493428c19ec0afe2e10e4a0d03e5783b7311906
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2024-05-11 07:15:55 +0800

    Try to fix view

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ function getClientHtml(domain) {
       <style>
         button {
           font-family: Consolas,"courier new";
-          font-size: 3vh;
+          font-size: 2.5vh;
         }
       </style>
     </head>
