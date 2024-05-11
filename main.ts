59d77d9de8f0abe7ff8f5d504145204e44fcedd3
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2024-05-11 14:57:24 +0800

    Change venue table padding

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ function getClientHtml(domain) {
         .venueTable {
           text-align: center;
           th, td {
-            padding-left: 3px; padding-right: 3px;
+            padding: 5px;
           }
           th {
             background-color: blanchedalmond;
