4295388aca1f132fd638548ad676cb1a8ab18acf
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2024-05-11 14:47:11 +0800

    Change  coloring

--- main.ts
+++ main.ts
@@ -10,6 +10,9 @@ function getClientHtml(domain) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Weekly Schedule</title>
       <style>
+        body {
+          background-color:floralwhite;
+        }
         table {
           td, th {border: 1px solid black;}
           border: 1px solid black;
